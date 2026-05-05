7d0ebf0bedd5a472a1c21301d16ffe8cf4a379fd
specimen <sanatrvaidya@gmail.com>
2026-05-05 16:32:45 +0530

    changed definiton of set_of_stated, added: add_to_set, in_set, copy_set

--- include/utils.h
+++ include/utils.h
@@ -1,16 +1,33 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-#define MaxStates 100
+#include <stdint.h>
+
+#define MaxStates 1024
 #define MaxSizeOfAlphabet 30
 #define NUMSTATE 5
 #define MAXREGEXLEN 200
+#define StatesPerWord 64
+#define WordsNeeded (MaxStates/StatesPerWord)
 
 //Definiton of common structs
-typedef struct set_of_states{ //A structure that contains a list of states and the count
+/*typedef struct set_of_states{ //A structure that contains a list of states and the count
 	int count;
 	int states[MaxStates];
 }set_of_states;
+*/
+
+typedef struct set_of_states{
+  uint64_t words[WordsNeeded];
+} set_of_states;
+
+void copy_set(struct set_of_states *dest,const set_of_states src);
+int in_states(int states[],int count,int current_state);
+int in_final(struct set_of_states final_states,int current_state);
+int get_position(struct set_of_states states, int state_value);
+void add_to_set(struct set_of_states *set, unsigned int state);
+int in_set(const struct set_of_states *set, unsigned int states);
+
 
 typedef struct set_of_set{
 	int count;
@@ -24,20 +41,16 @@ typedef struct alphabet{ //A structure that contains a list of states and the co
 
 //----------------------------------------------------
 
+int get_index(struct alphabet alphabet, char ch);
 void swap(int *a, int *b);
 int partition(int arr[], int low, int high);
 void quick_sort(int arr[], int low, int high);
 void swap_c(char *a, char *b);
 int partition_c(char arr[], int low, int high);
 void quick_sort_c(char arr[], int low, int high);
-void copy_states(struct set_of_states *dest, int states[], int size);
 void copy_table(int dest[][MaxSizeOfAlphabet], int src[][MaxSizeOfAlphabet], int row_size, int col_size);
 void copy_alphabet(struct alphabet *dest, struct alphabet src);
-int in_states(int states[],int count,int current_state);
 void sort_states(struct set_of_states *set);
-int in_final(struct set_of_states final_states,int current_state);
-int get_position(struct set_of_states states, int state_value);
-int get_index(struct alphabet alphabet, char ch);
 int precedence(char ch);
 int is_op(char ch);
 char *infix_to_postfix(char *in);
--- src/utils.c
+++ src/utils.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdint.h>
 
 //--------sorting codes--------------
 void swap(int *a, int *b){
@@ -69,13 +70,6 @@ void quick_sort_c(char arr[], int low, int high){
 //----------------------------------------------------
 
 //copying stuff
-void copy_states(struct set_of_states *dest, int states[], int size){
-	dest->count = size;
-	for(int i = 0; i<size; i++){
-		dest->states[i] = states[i];
-	}
-}
-
 void copy_table(int dest[][MaxSizeOfAlphabet], int src[][MaxSizeOfAlphabet], int row_size, int col_size){
 	for(int i = 0; i<row_size; i++){
 		for(int j =0; j<col_size;j++){
@@ -92,7 +86,41 @@ void copy_alphabet(struct alphabet *dest, struct alphabet src){
 }
 //----------------------------------------------------
 
+int get_index(struct alphabet alphabet, char ch){
+  //Currently this functions returns the corresponding index for a given input symbol
+	int size = alphabet.count;
+	for(int i = 0; i<size; i++){
+		if(alphabet.symbols[i] == ch) return i;
+	}
+	
+	return -1;
+}
+
+
 //----Opetations with states, set of states------------------------
+
+  /*Since im going to be trying to change the fundamental implemention of 
+   * set_of_states, i will need a lot of functions to go along with it.
+    each set_of_states function will be represented by set_<operation>
+  */
+
+void add_to_set(struct set_of_states *set, unsigned int state){
+  set->words[state/StatesPerWord] |= (1ULL << state%StatesPerWord);
+}
+
+int in_set(const struct set_of_states *set, unsigned int state) {
+    // 1. Shift instead of division (64 = 2^6)
+    // 2. Mask instead of modulo (state % 64 is the same as state & 63)
+    // 3. Return 1 or 0 explicitly
+    return (set->words[state >> 6] & (1ULL << (state & 63))) != 0;
+}
+
+void copy_set(struct set_of_states *dest, const struct set_of_states src){
+  for(int i = 0; i<WordsNeeded; i++){
+    dest->words[i] = src.words[i];
+  }
+}
+
 
 int in_states(int states[],int count,int current_state){
 	for(int i = 0;i<count;i++){
@@ -103,37 +131,30 @@ int in_states(int states[],int count,int current_state){
 }
 
 void sort_states(struct set_of_states *set){
-	quick_sort(set->states,0,set->count-1);
+	/*quick_sort(set->states,0,set->count-1);*/
 }
 
 //checks if a given state is in the set of final states
 int in_final(struct set_of_states final_states,int current_state){
-	
+  /*	
 	for(int i = 0;i <final_states.count;i++){
 		if(final_states.states[i] == current_state) return 1;
 	}
-	
+  */	
 	return 0;
 }
 
 int get_position(struct set_of_states states, int state_value){
 	//gets the corresponding index for the value of a state
+    /*
     for(int i = 0; i < states.count; i++){
         if(states.states[i] == state_value) return i;
     }
+    */
     return -1;
 }
 
-
-int get_index(struct alphabet alphabet, char ch){
-  //Currently this functions returns the corresponding index for a given input symbol
-	int size = alphabet.count;
-	for(int i = 0; i<size; i++){
-		if(alphabet.symbols[i] == ch) return i;
-	}
-	
-	return -1;
-}
+//-------------------------------------------------------------------------------------------
 
 //infix to postfix
 int precedence(char ch){
